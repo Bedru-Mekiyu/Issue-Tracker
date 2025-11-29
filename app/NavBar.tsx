5beb643dca63e63d0c73926040ba1557d37b0ddd
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-11-30 02:30:09 +0300

    install the react icons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
-import React from 'react'
+import { AiFillBug } from "react-icons/ai";
+
 
 function NavBar() {
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'>logo</Link>
+        <Link href='/'><AiFillBug /></Link>
         <ul className='flex space-x-6 '>
             <li>
-                <Link href='/'>Dashboard</Link>
+                <Link className='text-zinc-500 hover:text-zinc-800' href='/'>Dashboard</Link>
             </li>
             <li>
                 <Link href='/Issue'>Issues</Link>
