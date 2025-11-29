8144439fe8f211aafbe70afc2227be92cd51774f
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-11-30 02:43:02 +0300

    build the nav using dynamic

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,16 +3,18 @@ import { AiFillBug } from "react-icons/ai";
 
 
 function NavBar() {
+    const links=[
+        {label:'Dashboard',href:'/'},
+        {label:'Issues',href:'/Issues'}
+    ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'><AiFillBug /></Link>
+        <Link href='/'><AiFillBug  /></Link>
         <ul className='flex space-x-6 '>
-            <li>
-                <Link className='text-zinc-500 hover:text-zinc-800' href='/'>Dashboard</Link>
-            </li>
-            <li>
-                <Link href='/Issue'>Issues</Link>
-            </li>
+
+            {links.map((link)=><Link key={link.href} className='text-zinc-500 hover:text-zinc-800' href={link.href}>{link.label}</Link>)
+}
+        
         </ul>
     </nav>
   )
