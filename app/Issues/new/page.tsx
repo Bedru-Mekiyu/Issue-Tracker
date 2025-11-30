61438b220b1001c32a7fcf70ab9a2b9e85b2a62c
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-11-30 18:45:38 +0300

    handle errors using radix ui

--- app/Issues/new/page.tsx
+++ app/Issues/new/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import {useForm,Controller} from 'react-hook-form'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 
 interface IssueForm{
@@ -15,14 +16,25 @@ interface IssueForm{
 function NewIssuePage() {
    const router=useRouter()
  const {register,control,handleSubmit}= useForm<IssueForm>();
+ const [error,setError]=useState()
  
 
   return (
+    <div>
+      {error&& 
+      <Callout.Root color='red' className='mb-5'>
+        <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>}
     <form 
     onSubmit={handleSubmit(
      async (data)=>{
-        await axios.post('/api/issues',data);
+      try {
+          await axios.post('/api/issues',data);
         router.push('/Issues');
+      } catch (error) {
+        setError("unexpected error occure")
+      }
+      
       })
 
     }
@@ -38,6 +50,7 @@ render={(field)=>(<SimpleMDE placeholder='Description' {...field}/>) }
 
 <Button>Submit New Issue</Button>
 </form>
+</div>
   )
 }
 
