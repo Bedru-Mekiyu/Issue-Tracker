d3a0c7b8260b31c52ec42ac24bbb109bdbf0095a
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-12-01 14:00:43 +0300

    adding costume loading for every page

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -1,22 +1,19 @@
-import React, { forwardRef, Children } from 'react';
+'use client';
+
+import React, { forwardRef } from 'react';
 import { Link as RadixLink } from '@radix-ui/themes';
 import NextLink from 'next/link';
 
-interface LinkProps {
+interface LinkProps extends React.ComponentProps<typeof RadixLink> {
   href: string;
-  children: React.ReactNode;
-  className?: string;
-  
 }
 
 const Link = forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ href, children, className, ...props }, ref) => {
+  ({ href, children, ...props }, ref) => {
     return (
-      <NextLink href={href} passHref legacyBehavior>
-        <RadixLink ref={ref} className={className} {...props}>
-          {children}
-        </RadixLink>
-      </NextLink>
+      <RadixLink asChild ref={ref} {...props}>
+        <NextLink href={href}>{children}</NextLink>
+      </RadixLink>
     );
   }
 );
