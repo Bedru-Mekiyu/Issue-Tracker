d8fca792a7bb4f17082f2eabec55c75adebbb216
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-12-02 14:00:39 +0300

    apply the single responsibility

--- app/Issues/[id]/page.tsx
+++ app/Issues/[id]/page.tsx
@@ -5,6 +5,8 @@ import ReactMarkdown from "react-markdown";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { Pencil2Icon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import { EditIssueButton } from "./EditIssueButton";
+import { IssueDetails } from "./IssueDetails";
 
 interface Props {
   params: Promise<{ id: string }>; // important for Next 15/16 with Turbopack
@@ -29,22 +31,10 @@ const IssueDetailPage = async ({ params }: Props) => {
   return (
     <Grid columns={{initial:'1',md:'2'}} gap='5  '>
       <Box>
-      <Heading>{issue.title}</Heading>
-      
-      <Flex className="space-x-3" my='2'> 
-        <IssueStatusBadge status={issue.status}/>
-      <Text>{issue.createdAt.toDateString()}</Text>
-
-      </Flex>
-      <Card className="prose " mt='4'>
-     <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
+      <IssueDetails issue={issue}/>
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon/>
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-          </Button>
+       <EditIssueButton issueId={issue.id}/>
       </Box>
      
     </Grid>
