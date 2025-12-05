c5a9b383ad8c0353fbc66c976b288b93e2d99b56
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-12-06 00:30:06 +0300

    build filtering

--- app/Issues/list/IssueActions.tsx
+++ app/Issues/list/IssueActions.tsx
@@ -1,12 +1,14 @@
-import { Button } from '@radix-ui/themes'
+import { Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
+import { IssueStatusFilter } from './IssueStatusFilter'
 
 const IssueActions = () => {
   return (
-      <div className="mb-5">
+      <Flex mb='5' justify='between' align='center'>
+        <IssueStatusFilter />
        <Button><Link href='/Issues/new'>new issue</Link></Button>
-       </div>
+       </Flex>
   )
 }
 
---
+++ app/Issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Status } from '@prisma/client';
+import { Select } from '@radix-ui/themes'
+import React from 'react'
+type StatusFilterValue = Status | "ALL";
+export const IssueStatusFilter = () => {
+    const statuses:{label:string, value?:StatusFilterValue }[] = [
+        {label: "All", value: "ALL"},
+        {label: "Open", value: "OPEN"},
+        {label: "In Progress", value: "IN_PROGRESS"},
+        {label: "Closed", value: "CLOSED"},
+    ];
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+     {   statuses.map(status => (
+        <Select.Item key={status.value} value={status.value || ''}>
+          {status.label}
+        </Select.Item>
+      ))}
+      </Select.Content>
+    </Select.Root>
+  )
+}
